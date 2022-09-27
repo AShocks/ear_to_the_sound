6ddfa707769cae0a02030c329054cbfced88c845
AShocks <oshaughnessy.adrian@gmail.com>
2022-09-27 03:30:56 +0000

    Add playlist model to admin panel

--- playlist/admin.py
+++ playlist/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Playlist
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Playlist)
+class PlaylistAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('note_on_songs')
+
+
