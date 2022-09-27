7d91b6168957ec69c538479452273ba693397c6a
AShocks <oshaughnessy.adrian@gmail.com>
2022-09-27 02:37:21 +0000

    Create models for playlist and comments in models.py

--- playlist/models.py
+++ playlist/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="playlist_posts"
+    )
+    updated_on = models.DateTimeField(auto_now=True)
+    playlist_image = CloudinaryField('image', default='placeholder')
+    songs = models.TextField(blank=False)
+    note_on_songs = models.TextField(blank=True, max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+                                User,
+                                related_name='blogpost_like',
+                                blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    playlist = models.ForeignKey(
+        Playlist, 
+        on_delete=models.CASCADE,
+        related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
