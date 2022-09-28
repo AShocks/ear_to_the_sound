e5b24a3e046011bf56b77de9265a0ff181ae172d
AShocks <oshaughnessy.adrian@gmail.com>
2022-09-28 01:34:49 +0000

    Edit code in base and html files

--- playlist/views.py
+++ playlist/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Playlist
+from django.views import generic, View
+from .models import Post
 
 
-class PlayList(generic.ListView):
-
-    model = Playlist
-    queryset = Playlist.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "post_list.html"
+    paginate_by = 6
